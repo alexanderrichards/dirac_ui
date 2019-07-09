38312d6a6e3becfcdc440bdfe5d4ecfe16f7b693
Alexander Richards <a.richards@imperial.ac.uk>
2019-07-09 14:52:55 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run with ID=$ID ./build.sh
-DOCKER_BUILDKIT=1 docker build --no-cache --progress=plain --secret id=proxy,src=/tmp/x509up_u$ID --build-arg dirac_version="$1" --tag="alexanderrichards/dirac_ui:${1}-test" .
-#DOCKER_BUILDKIT=1 docker build --progress=plain --secret id=proxy,src=/tmp/x509up_u$ID --build-arg dirac_version="$1" --tag="alexanderrichards/dirac_ui:latest" .
+DOCKER_BUILDKIT=1 docker build --no-cache --progress=plain --secret id=proxy,src=/tmp/x509up_u$ID --build-arg dirac_version="$1" --tag="alexanderrichards/dirac_ui:$1" .
+DOCKER_BUILDKIT=1 docker build --progress=plain --secret id=proxy,src=/tmp/x509up_u$ID --build-arg dirac_version="$1" --tag="alexanderrichards/dirac_ui:latest" .
